eaab447d079c37cd52522e474987018b700cec60
Atulpriya Sharma <sharma.atulpriya@gmail.com>
2025-06-12 19:05:57 +0530

    Update k6-sample.js

--- test/k6/k6-sample.js
+++ test/k6/k6-sample.js
@@ -1,6 +1,6 @@
 import http from "k6/http";
 
 export default function () {
-  http.get('http://nginx-service.default.svc.cluster.local');
+  http.get('http://nginx-service.nginx.svc.cluster.local');
 }
    
